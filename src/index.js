fe3c668134bc74682c67322b65ea7b9b2e6ee7d6
shivangijb <37670360+shivangijb@users.noreply.github.com>
2020-11-22 09:41:46 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
+  httpServer.post('/welcome', (req,res)=>{
+        res.status(200).send("Welcome to Dominos!");
+        return;
+        
+    }); 
+    httpServer.post('/contact', (req,res)=>{
+        const application = {
+            "phone": '18602100000',
+            "email": 'guestcaredominos@jublfood.com'
+        }
+        res.status(200).send(application);
+        return;
+    }); 
+    res.status(404);
   
 }
 
 module.exports = httpServer;
+httpServer.listen(8081);
