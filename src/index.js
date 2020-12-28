df3d1799835e3a98f4f6d82b4d0cc5c7c21f802c
shivangijb <shivangijain331@gmail.com>
2020-12-28 14:56:48 -0800

    ...

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
-var http = require("http");
-
+const http = require('http');
 const httpServer = http.createServer(handleServer);
 
 
-function handleServer(req, res) {
-  if(req.url ==="/welcome")
-    {   
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.write("Welcome to Dominos!");
-        res.end();
-    } 
-    else if(req.url ==="/contact")
-    {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.write(JSON.stringify({
-            phone: '18602100000',
-            email: 'guestcaredominos@jublfood.com'
-            }));
+function handleServer (req, res) {
+    if (req.url === '/welcome') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write('Welcome to Dominos!');
         res.end();
     }
-    else{
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end("404 Not Found");
-  
-}
-httpServer.listen(8081);
-module.exports = httpServer;
-   
 
+    else if (req.url === '/contact') {
+        const contact =  {
+            phone: '18602100000',
+            email: 'guestcaredominos@jublfood.com'
+        }
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify(contact));
+        res.end();
+    }
+    else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.write("404"); 
+        res.end();
+    }    
+    
+}
+
+module.exports = httpServer;
+console.log("Listening...");
+httpServer.listen(8081);
